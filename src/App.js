c0dc63479ae45f6d40476e1999c28a5976638164
josue g <josuegallardolara@hotmail.com>
2021-01-07 21:28:10 -0800

    adding mosnter to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {CardList} from './components/card-list/card-list.component';
+import { render } from '@testing-library/react';
+import {SearchBox} from './components/search-box/search-box.component';
 
-function App() {
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      monsters:[],
+      searchField:''
+    };
+  }
+
+  //using mount everytime the app opens,
+  //converting the website into json
+  //move the name from the json into monster[]
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users') 
+  .then(response => response.json())
+  .then(users=> this.setState({monsters:users}))
+}
+
+handleChange = e =>{
+  this.setState({searchField: e.target.value});
+}
+
+render() {
+  const {monsters, searchField}=this.state;
+  //same as const monsters=this.state.monsters
+  //const searchField= this.state.seachField
+  const filteredMonsters=monsters.filter
+  (monster =>
+    monster.name.toLowerCase().includes
+    (searchField.toLocaleLowerCase()));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monsters Search Box</h1>
+      <SearchBox 
+        placeholder='search for a monster'
+        handleChange={this.handleChange}
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
+}
 }
 
 export default App;
